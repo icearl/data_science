c9ff265db4eb06d53762e2a7b687b94e88b7c155
icearl <icearl@qq.com>
2018-05-22 11:21:19 +0800

    update

---
+++ lib/preprocess/drop_null.py
@@ -0,0 +1,25 @@
+def drop_null_str(string):
+    """
+    去掉 list（内部为str）里的空str（''）
+    :param string:
+    :return:
+    """
+    str_list = string.split(',')
+    new_list = []
+#     print(str_list)
+    for i in str_list:
+        # if i != '' and i != 'android':
+        if i != '':
+            new_list.append(i)
+    return ' '.join(new_list)
+
+def drop_nan(df, app_name):
+    """
+    去空
+    :param df:
+    :param app_name:applist 的字段名
+    :return:
+    """
+    df = df.dropna(axis=0, how='any')
+    df[app_name] = df[app_name].apply(drop_null_str)
+    return df
---
+++ lib/preprocess/split.py
@@ -0,0 +1,13 @@
+def split(df, ritio_of_train):
+    """
+
+    :param df:
+    :param ritio_of_train:
+    :return:
+    """
+    df = df.sample(frac=1.0)  # 全部打乱
+    cut_idx = int(round(ritio_of_train * df.shape[0]))
+    df_train, df_test = df.iloc[:cut_idx], df.iloc[cut_idx:]
+    # df_train.to_csv('df_train.csv', encoding="utf-8")
+    # df_test.to_csv('df_test.csv', encoding="utf-8")
+    return df_train, df_test
